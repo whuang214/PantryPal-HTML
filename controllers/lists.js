f27f89d6baad7fb650730a27e38509519a8ac686
whuang214 <whuang2146@gmail.com>
2023-07-11 10:48:34 -0400

    add styles to my list page

--- controllers/lists.js
+++ controllers/lists.js
@@ -8,10 +8,11 @@ async function allUserList(req, res) {
     const user = req.user;
 
     if (user) {
+        console.log('user->', user);
         const allUserGrocceryLists = await List.find({ owner: user._id });
         res.render('lists/index', { title: 'All Lists', lists: allUserGrocceryLists });  
     } else {
-        // not logged in
+        console.log('not logged in');
         res.redirect('/');
     }
 }
