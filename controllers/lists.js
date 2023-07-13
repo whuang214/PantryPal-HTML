3c77fc3ac5de33c3ae83534aedc4ea800d6d3b75
whuang214 <whuang2146@gmail.com>
2023-07-13 16:14:39 -0400

    add delete shared user feature

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,144 +1,170 @@
-const List = require('../models/GroceryList');
+const List = require("../models/GroceryList");
 
 module.exports = {
-    allUserList,
-    new: newlist,
-    create,
-    show,
-    addItem,
-    deleteList,
-    deleteItem,
-    editPage,
-    update,
+  allUserList,
+  new: newlist,
+  create,
+  show,
+  addItem,
+  deleteList,
+  deleteItem,
+  editPage,
+  update,
+  deleteSharedUser,
 };
 
 // render index page
 async function allUserList(req, res) {
-    const user = req.user;
-  
-    if (user) {
-      try {
-        const ownedLists = await List.find({ owner: user._id }).populate('owner').exec();
+  const user = req.user;
 
-        const sharedLists = await List.find({ sharedList: { $in: [user._id] } }).populate('owner').exec();
-        console.log('sharedLists->', sharedLists);  
-        const allLists = ownedLists.concat(sharedLists);
-  
-        res.render('lists/index', { title: 'My Lists', lists: allLists });
-      } catch (err) {
-        console.log(err);
-        res.redirect('/');
-      }
-    } else {
-      console.log('not logged in');
-      res.redirect('/');
+  if (user) {
+    try {
+      const ownedLists = await List.find({ owner: user._id })
+        .populate("owner")
+        .exec();
+
+      const sharedLists = await List.find({ sharedList: { $in: [user._id] } })
+        .populate("owner")
+        .exec();
+      console.log("sharedLists->", sharedLists);
+      const allLists = ownedLists.concat(sharedLists);
+
+      res.render("lists/index", { title: "My Lists", lists: allLists });
+    } catch (err) {
+      console.log(err);
+      res.redirect("/");
     }
+  } else {
+    console.log("not logged in");
+    res.redirect("/");
   }
-  
-  
+}
+
 // new list page
 function newlist(req, res) {
-    res.render('lists/new', { title: 'Create List' });
+  res.render("lists/new", { title: "Create List" });
 }
 
 // create list
 async function create(req, res) {
-    const list = new List(req.body);
-    list.owner = req.user._id;
-    console.log('list->', list);
-    try {
-        await list.save();
-        res.redirect('/lists');
-    } catch (err) {
-        console.log(err);
-        res.render('lists/new', { title });
-    }
+  const list = new List(req.body);
+  list.owner = req.user._id;
+  console.log("list->", list);
+  try {
+    await list.save();
+    res.redirect("/lists");
+  } catch (err) {
+    console.log(err);
+    res.render("lists/new", { title });
+  }
 }
 
 //show list
 async function show(req, res) {
-    try {
-        const list = await List.findById(req.params.id).populate('owner').populate('itemsList').populate('sharedList').exec();
-        // console.log('list->', list);
-        res.render('lists/show', { title: 'List Details', list });
-    } catch (err) {
-        console.log(err);
-        res.redirect('/lists');
-    }
+  try {
+    const list = await List.findById(req.params.id)
+      .populate("owner")
+      .populate("itemsList")
+      .populate("sharedList")
+      .exec();
+    // console.log('list->', list);
+    res.render("lists/show", { title: "List Details", list });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/lists");
+  }
 }
 
 // add item to list
 async function addItem(req, res) {
-    try {
-        const list = await List.findById(req.params.id);
-        list.items.push(req.body);
-        await list.save();
-        res.redirect(`/lists/${list._id}`);
-    } catch (err) {
-        console.log(err);
-        res.redirect(`/lists/${list._id}`);
-    }
+  try {
+    const list = await List.findById(req.params.id);
+    list.items.push(req.body);
+    await list.save();
+    res.redirect(`/lists/${list._id}`);
+  } catch (err) {
+    console.log(err);
+    res.redirect(`/lists/${list._id}`);
+  }
 }
 
 // delete list
 async function deleteList(req, res) {
-    try {
-        await List.findByIdAndDelete(req.params.id);
-        res.redirect('/lists');
-    } catch (err) {
-        console.log(err);
-        res.redirect('/lists');
-    }
+  try {
+    await List.findByIdAndDelete(req.params.id);
+    res.redirect("/lists");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/lists");
+  }
 }
 
 // delete item from list
 async function deleteItem(req, res) {
-    try {
-        const list = await List.findById(req.params.id);
-        const idx = list.itemsList.findIndex(item => item.id === req.params.itemId);
-        list.itemsList.splice(idx, 1);
-        await list.save();
-        res.redirect(`/lists/${list._id}`);
-    } catch (err) {
-        console.log(err);
-        res.redirect(`/lists/${list._id}`);
-    }
+  try {
+    const list = await List.findById(req.params.id);
+    const idx = list.itemsList.findIndex(
+      (item) => item.id === req.params.itemId
+    );
+    list.itemsList.splice(idx, 1);
+    await list.save();
+    res.redirect(`/lists/${list._id}`);
+  } catch (err) {
+    console.log(err);
+    res.redirect(`/lists/${list._id}`);
+  }
+}
+
+// delete shared user from list
+async function deleteSharedUser(req, res) {
+  try {
+    const list = await List.findById(req.params.id); // get list from db
+    const idx = list.sharedList.findIndex( // find index of user in sharedList array
+      (item) => item.id === req.params.userId
+    );
+    console.log("idx->", idx);
+    list.sharedList.splice(idx, 1);
+    await list.save();
+    res.redirect(`/lists/${list._id}`);
+  } catch (err) {
+    console.log(err);
+    res.redirect(`/lists/${list._id}`);
+  }
 }
 
 // render edit page
 async function editPage(req, res) {
-    try {
-      const list = await List.findById(req.params.id)
-        .populate('owner')
-        .populate('sharedList')
-        .exec();
-  
-      res.render('lists/edit', { title: 'Edit List', list });
-    } catch (err) {
-      console.error(err);
-      res.redirect('/lists');
-    }
+  try {
+    const list = await List.findById(req.params.id)
+      .populate("owner")
+      .populate("sharedList")
+      .exec();
+
+    res.render("lists/edit", { title: "Edit List", list });
+  } catch (err) {
+    console.error(err);
+    res.redirect("/lists");
   }
-  
+}
 
 // update list
 async function update(req, res) {
-    try {
-      const listId = req.params.id;
-      const updatedList = await List.findByIdAndUpdate(
-        listId,
-        {
-          title: req.body.title,
-          // Add other fields to update
-        },
-        { new: true }
-      );
-      if (!updatedList) {
-        throw new Error('List not found');
-      }
-      res.redirect(`/lists/${updatedList._id}`);
-    } catch (err) {
-      console.error(err);
-      res.redirect('/lists');
+  try {
+    const listId = req.params.id;
+    const updatedList = await List.findByIdAndUpdate(
+      listId,
+      {
+        title: req.body.title,
+        // Add other fields to update
+      },
+      { new: true }
+    );
+    if (!updatedList) {
+      throw new Error("List not found");
     }
+    res.redirect(`/lists/${updatedList._id}`);
+  } catch (err) {
+    console.error(err);
+    res.redirect("/lists");
   }
+}
--- routes/lists.js
+++ routes/lists.js
@@ -4,28 +4,31 @@ const listsCtrl = require('../controllers/lists');
 // render all lists
 router.get('/', listsCtrl.allUserList);
 
-// GET /lists/new
+// GET /lists/new page
 router.get('/new', listsCtrl.new);
 
-// POST /lists
+// POST a new list
 router.post('/create', listsCtrl.create);
 
-// GET /lists/:id
+// GET specific list id
 router.get('/:id', listsCtrl.show);
 
-// POST /lists/:id/items
+// POST item to a list
 router.post('/:id/items', listsCtrl.addItem);
 
-// // DELETE /lists/:id
+// DELETE list from user
 router.delete('/:id', listsCtrl.deleteList);
 
-// DELETE /lists/:id/items/:itemId
+// DELETE item from list
 router.delete('/:id/item/:itemId', listsCtrl.deleteItem);
+
+// DELETE shared user from list
+router.delete('/:id/shared/:userId', listsCtrl.deleteSharedUser);
 
 // GET /lists/:id/edit
 router.get('/:id/edit', listsCtrl.editPage);
 
-// PUT /lists/:id
+// PUT the edited list
 router.put('/:id/edit', listsCtrl.update);
 
 module.exports = router;
